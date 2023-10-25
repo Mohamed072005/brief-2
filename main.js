9f98c6e8be235d5f40660e03732c04fa4c74ddf5
Mohamed072005 <amine072005@gmail.com>
2023-10-25 20:20:25 +0100

    edit the burger manu in all pages and edit the js for remove the product and calculating the number of products

--- main.js
+++ main.js
@@ -1,35 +1,130 @@
+// JS for panier page 
+
+var carts = document.getElementsByClassName('edit-product-info');
+let plusButton = document.getElementsByClassName('plus-button');
+var moinButton = document.getElementsByClassName('moins-button');
+let productPrice = document.querySelectorAll('#product-price');
+var inputButton = document.getElementsByClassName('input');
+var productPricePlus = document.querySelectorAll('#product-price-plus');
+var totalPrice = document.querySelector('#total-price');
+var removeButton = document.querySelectorAll('.remove-button');
+var productRemove = document.querySelectorAll('.edit-product-info');
+let carts_length = carts.length;
+let displayPrice;
+let removePrice;
 
 
-document.getElementById('input-quantity-value-master').value = 1;
-document.getElementById('input-quantity-value-1').value = 1;
-document.getElementById('input-quantity-value-2').value = 1;
-document.getElementById('input-quantity-value-3').value = 1;
 
 
-let quantity = document.getElementById('input-quantity-value-master').value;
-let totalPrice = 0;
+for(let i = 0; i < carts_length; i++){
+    plusButton[i].addEventListener('click', function () {
+        inputButton[i].value++;
+        displayPrice = inputButton[i].value * Number(productPrice[i].innerText);
+        productPricePlus[i].textContent = displayPrice.toFixed(2);
+        totalcheckout();
+    })
+}   
 
-document.getElementById('total-price').innerText = (quantity * 20.00) + 84.00 + ' $';
-
-function pressPlus () {
-    document.getElementById('input-quantity-value-master').value++;
-    quantity = document.getElementById('input-quantity-value-master').value;
-    totalPrice = (quantity * 20.00) + 84.00;
-    document.getElementById('total-price').innerText = totalPrice + ' $';
+for(let i = 0; i < carts_length; i++){
+    moinButton[i].addEventListener('click', function () {
+        if(inputButton[i].value > 1){
+            inputButton[i].value--;
+            displayPrice = inputButton[i].value * Number(productPrice[i].innerText); 
+            productPricePlus[i].textContent = displayPrice.toFixed(2);            
+        }
+        totalcheckout();
+        
+    })
 }
 
-function pressMoins () {
-    if(quantity > 1){
-        document.getElementById('input-quantity-value-master').value--;
-        quantity = document.getElementById('input-quantity-value-master').value;
-        totalPrice = (quantity * 20.00) + 84.00;
-        document.getElementById('total-price').innerText = totalPrice  + ' $';
+for(let i = 0; i < carts_length; i++){
+    removeButton[i].addEventListener('click', function() {
+        productRemove[i].style.display = 'none';
+        totalcheckout();
+    })
+    
+}
+
+function totalcheckout(){
+    let summ = 0;
+    for(let i = 0; i<carts_length; i++){
+        if(productRemove[i].style.display != 'none'){
+            summ += Number(productPrice[i].innerHTML) * inputButton[i].value;
+            totalPrice.innerText = summ + ' $';
+        }
     }
 }
 
-function pressRemove () {
-    document.getElementById('remove').style.display = 'none';
-    let removePrice = quantity * 20.00;
-    totalPrice = (quantity * 20.00) + 84.00;
-    document.getElementById('total-price').innerText = totalPrice - removePrice + ' $';
+
+
+
+
+// JS for menu !!!!!!
+
+function displayMenu () {
+    document.querySelector('button').style.display = 'none';
+    document.getElementById('menu-burger').style.display = 'block';
+    document.getElementsByClassName('close-button')[0].style.display = 'block'
 }
+
+function removeMenu () {
+    document.querySelector('button').style.display = 'block';
+    document.getElementById('menu-burger').style.display = 'none';
+    document.getElementsByClassName('close-button')[0].style.display = 'none'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
